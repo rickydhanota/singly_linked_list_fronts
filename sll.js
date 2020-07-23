d096e24cf86ab609937010ff5caf2f648b6144ad
Ricky Dhanota <rickdhanota@gmail.com>
2020-07-22 21:47:04 -0700

    adding a new class

--- sll.js
+++ sll.js
@@ -7,6 +7,25 @@ class Node{
         console.log("I am Node", this.value)
     }
 }
+
+//single linked link class
+
+class SLL{
+    constructor(val){
+        this.head= Node(val);
+    }
+    addNode(val){
+
+    }
+    removeNode(val){
+
+    }
+}
+
+sll=SLL();
+n1=new Node(12);
+
+
 n1=new Node(12);
 console.log(n1.value)
 n1.display()
