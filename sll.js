6be317abb5604c554346fe6b9e17cfb49aef030c
Ricky Dhanota <rickdhanota@gmail.com>
2020-07-22 23:05:33 -0700

    adding another class

--- sll.js
+++ sll.js
@@ -12,20 +12,35 @@ class Node{
 
 class SLL{
     constructor(val){
-        this.head= Node(val);
+        this.head= new Node(val);
     }
     addNode(val){
-
+        if (this.head==null){ //This if statement is part of the removeNode as well as front function as well
+            this.head = new Node(val);
+            return this
+        }
+        else{
+            var restOfList = this.head;
+            this.head = new Node(val);
+            this.head.next = restOfList;
+            return this
+        }
     }
     removeNode(val){
-
+        this.head==this.head.next;
+        return this
+    }
+    front(){
+        this.head.value;
+        return this //return this.head.value or can we do return this?
     }
 }
 
-sll=SLL();
-n1=new Node(12);
 
-
+sll= new SLL();
 n1=new Node(12);
+n2=new Node(13);
+n3=new Node(14);
+
 console.log(n1.value)
 n1.display()
